886812b7259bcd7b08ac8b5f453b108015087986
B20-Project <dilyar.aji@hotmail.com>
2020-10-15 21:30:08 +0200

    clean up

--- src/test/java/Sprint3/tests/Message_Tab.java
+++ src/test/java/Sprint3/tests/Message_Tab.java
@@ -3,17 +3,11 @@ package Sprint3.tests;
 import Sprint3.util.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class Message_Tab {
